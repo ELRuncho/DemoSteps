7e3363bf4816a76bd9c02d13b6dc05b247ef0e3d
ElRuncho <francorafae@gmail.com>
2021-08-09 14:35:09 -0500

    sample translator

---
+++ bam_demo_steps/resources/traductorxml.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Catalogo') 
+def lambda_handler(event, context):
+    # TODO implement
+    response = table.get_item(
+        Key={
+                'TipoTransaccion': event['tipo']
+        }
+    )
+    item = response['Item']
+    return item#{
+        #'statusCode': 200,
+        #'body': json.dumps('Hello from Lambda!')
+    #}
