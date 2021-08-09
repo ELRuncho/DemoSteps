58ca3b11678934225409331591041fc228751af5
ElRuncho <francorafae@gmail.com>
2021-08-09 09:32:53 -0500

    typo

--- bam_demo_steps/resource_stacks/customVpc.py
+++ bam_demo_steps/resource_stacks/customVpc.py
@@ -3,7 +3,6 @@ from aws_cdk import core as cdk
 
 class CustomVPCStack():
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        
